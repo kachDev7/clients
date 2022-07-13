6f7306632652b7e12ecd0cad7ee3dd65013912ea
kachDev7 <99486031+kachDev7@users.noreply.github.com>
2022-07-13 13:58:46 +0100

    mine

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react"
 import jwt from 'jsonwebtoken'
+import Image from "next/image";
 
 const Profile = () => {
 
@@ -50,7 +51,7 @@ const Profile = () => {
     const Transfer = () => {
         return(
             <div className="container">
-                <h1>Transfer</h1>
+                <h1 className="mb-2">Transfer</h1>
                 <Table users={sentAmount} />
             </div>
         )
@@ -58,7 +59,7 @@ const Profile = () => {
     const Recieved = () => {
         return(
             <div className="container">
-                <h1>Recieved</h1>
+                <h1 className="mb-2">Recieved</h1>
                 <Table users={recieved} />
             </div>
         )
@@ -67,18 +68,23 @@ const Profile = () => {
     // Table
     const Table = ({users}) => {
         return(
-            <table className="table table-primary table-striped br-20">
-                <thead>
-                    <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Amount($)</th>
-                    <th scope="col">Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <InnerTable users={users} />
-                </tbody>
-            </table>
+            <div className="mb-5">
+
+            
+                <table className="table table-primary table-striped br-20">
+                    <thead>
+                        <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Amount($)</th>
+                        <th scope="col">Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <InnerTable users={users} />
+                    </tbody>
+                </table>
+
+            </div>
         )
     }
     const InnerTable = ({users}) => {
@@ -97,10 +103,43 @@ const Profile = () => {
         )
     }
 
+    // Account Details
+    const Details = () => {
+        return(
+            <div className="px-2 myBox br-20 bg-body my-5">
+                <div className="box-holder d-flex jaa">
+                    {/* Profile Image */}
+                    <div className="d-flex jac r-boda column my-3 text-center">
+                        <div className="">
+                            <p className="text-secondary"><i className="bi bi-person mx-1"></i>Account Name</p>
+                            <h4 className="text-center">{userData.name}</h4>
+                        </div>
+                        <div className="px-3">
+                            <p className="mt-2 text-secondary"><i className="bi bi-credit-card-2-back mx-1"></i>Account Number</p>
+                            <h4 className="text-center">123456799</h4>
+                        </div>
+                    </div>
+
+                    {/* Profile Image */}
+                    <div className="d-flex jac column my-3 text-center">
+                        <div className="">
+                            <p className="text-secondary"><i class="bi bi-person-rolodex mx-1"></i>Swift Id</p>
+                            <h4 className="text-center">564323</h4>
+                        </div>
+                        <div className="px-3">
+                            <p className="mt-2 text-secondary"><i class="bi bi-shield-lock mx-1"></i>Secret PIN</p>
+                            <h4 className="text-center">****</h4>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
     return(
         <div className="container">
-            <h1>Name :{userData.name} </h1>
-            <h1>Amount :{balance} </h1>
+            <Details />
             <Transfer />
             <Recieved />
             
